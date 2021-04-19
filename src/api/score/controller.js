415214ae182df44815857a4b15207f4ff9280b13
Artem <arlaptiev@gmail.com>
2021-04-19 18:20:31 +0300

    init

---
+++ src/api/score/controller.js
@@ -0,0 +1,15 @@
+export const score = (req, res) => {
+  // ToDO
+  // const { username, password } = req.body
+  // authorize(username, password)
+  //   .then(unauthorized(res))
+  //   .then(function (isAuthorized) {
+  //     if (isAuthorized) {
+  //       return Job.create(req.body)
+  //     }
+  //     return null
+  //   })
+  //   .then(internalError(res))
+  //   .then(success(res))
+  return null
+}
