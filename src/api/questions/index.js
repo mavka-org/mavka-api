1d3576d7afe519dc9ba8931f02858a1c3dcb02b6
Artem <arlaptiev@gmail.com>
2021-05-15 02:24:21 +0300

    Docs expanded

--- src/api/questions/index.js
+++ src/api/questions/index.js
@@ -45,16 +45,36 @@ const router = new Router()
 router.post('/get', getQuestions)
 
 /**
- * TODO
- * @api {get} /auth Authenticate
- * @apiName Authenticate
- * @apiGroup Auth
- * @apiPermission master
- * @apiHeader {String} Authorization Basic authorization with email and password.
- * @apiParam {String} access_token Master access_token.
- * @apiSuccess (Success 201) {String} token User `access_token` to be passed to other requests.
- * @apiSuccess (Success 201) {Object} user Current user's data.
- * @apiError 401 Master access only or invalid credentials.
+ * @api {post} /questions/get Get Random Questions
+ * @apiName GetRandomQuestions
+ * @apiGroup Questions
+ * @apiParam {String} [subject]          Subject's id.
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "subject": "5ffaeae5415c8719e08414d4"
+ *     }
+ * @apiSuccess {Object[]} questions          List of questions for a requested test or topic.
+ * @apiSuccess {String}   questions._id      Question's id.
+ * @apiSuccess {Number}   questions.order_n  Question's order number within a test / topic.
+ * @apiSuccess {String}   questions.subject  Question's subject's id.
+ * @apiSuccess {String}   questions.question_type  Question's type name.
+ * @apiSuccess {String}   questions.primary_question  Question's primary question in html format.
+ * @apiSuccess {String[]} questions.tasks    List of question's tasks in html format.
+ * @apiSuccess {String[]} questions.options  List of question's options in html format.
+ * @apiSuccess {Object}   questions.active_explanation          Question's active explanation.
+ * @apiSuccess {String}   questions.active_explanation.comment  Question's overall comment.
+ * @apiSuccess {String[]} questions.active_explanation.option_explanations  List of question's explanations for options.
+ * @apiSuccess {String[]} questions.active_explanation.task_explanations    List of question's explanations for tasks.
+ * @apiSuccess {Object}   questions.test          The test question belongs to.
+ * @apiSuccess {String}   questions.test._id      Question's test's id.
+ * @apiSuccess {Number}   questions.test.order_n  Question's test's order number within a subject.
+ * @apiSuccess {String}   questions.test.session  Question's test's session name.
+ * @apiSuccess {Number}   questions.test.year     Question's test's year.
+ * @apiSuccess {Object[]} questions.topic         The topic question belongs to.
+ * @apiSuccess {String}   questions.topic._id     Question's topic's id.
+ * @apiSuccess {String}   questions.topic.name    Question's topic's name.
+ * @apiSuccess {Number}   questions.topic.order_n Question's topic's order number within a chapter.
+ * ToDo apiError
  */
 router.post('/getRandom', getRandomQuestions)
 
