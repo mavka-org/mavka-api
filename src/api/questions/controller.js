d04870b534e8d7f8315de688717705592e21581c
Artem <arlaptiev@gmail.com>
2021-05-19 15:00:15 +0300

    bug-fix: questions/getRandom

--- src/api/questions/controller.js
+++ src/api/questions/controller.js
@@ -12,9 +12,9 @@ export const getQuestions = (req, res) => {
 export const getRandomQuestions = (req, res) => {
   Question.aggregate([
     { $match: req.body }
-  ].concat(basicQuestionAggregator)).concat([
+  ].concat(basicQuestionAggregator).concat([
     { $sample: { size: req.query.sampleSize || 10 } }
-  ])
+  ]))
     .then(internalError(res))
     .then(success(res))
 }
